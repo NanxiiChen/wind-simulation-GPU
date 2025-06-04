ff1b35f824f6abafed269056683c57250adab7f9
Nanxi Chen <nxchen@tongji.edu.cn>
2025-06-04 17:14:58 +0800

    update init.py

--- stochastic_wind_simulate/__init__.py
+++ stochastic_wind_simulate/__init__.py
@@ -1,2 +1,21 @@
 from .model import WindSimulator
 from .visualizer import WindVisualizer
+
+
+__title__ = "Stochastic Wind Simulation"
+__version__ = "0.1.0"
+__author__ = "Nanxi Chen"
+__email__ = "nxchen@tongji.edu.cn"
+__license__ = "GPL-3.0"
+__copyright__ = "Copyright (c) 2025 Nanxi Chen"
+
+__all__ = [
+    "__title__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+    "WindSimulator",
+    "WindVisualizer",
+]
